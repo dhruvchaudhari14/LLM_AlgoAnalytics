eeb4e3ab1ddc8610355caadd743ba415522f4ba3
dhruvchaudhari14 <65155859+dhruvchaudhari14@users.noreply.github.com>
2023-07-28 12:47:53 +0530

    Update app_algo.py

--- app_algo.py
+++ app_algo.py
@@ -20,23 +20,26 @@ from langchain.vectorstores import Chroma
 
 import pickle
 
-# Step 1: Define the file path where you saved the document
-file_path = "data"
-
-# Step 2: Open the file in binary mode and use pickle to load the object
-with open(file_path, "rb") as file_path:
-    data = pickle.load(data)
 
 
 # loader = TextLoader("/kaggle/input/qatext-processed/final.txt")
 # data = loader.load()
 # data
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=10)
-texts = text_splitter.split_documents(data)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=10)
+# texts = text_splitter.split_documents(data)
+
+
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-final_embeddings= HuggingFaceEmbeddings()
-docsearch = FAISS.from_documents(texts, final_embeddings)
+
+
+# final_embeddings= HuggingFaceEmbeddings()
+# docsearch = FAISS.from_documents(texts, final_embeddings)
+
+docsearch = np.load(f"docs.npy", allow_pickle=True)
+docsearch = docsearch.item()
+# Access the value directly
+final_embeddings = final_embeddings.item()
 llm=HuggingFaceHub(repo_id="google/flan-t5-xxl",model_kwargs={"temperature": 0.4, "max_length": 500,"batch_size":32})
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":7})
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
